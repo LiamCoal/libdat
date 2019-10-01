aeab7b8b9f26b107b70c4e3d2940073bc4e8d7d2
liamiam <liam@liamiam.com>
2019-09-30 20:37:50 -0700

    Try using . instead of ~/build/libdat

--- travis/build.sh
+++ travis/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd ~/build/libdat
-cmake -S ~/build/libdat -B ~/build/libdat/build
-cmake --build ~/build/libdat/build
-~/build/libdat/build/dat_store test.dat build
-~/build/libdat/build/dat_read test.dat Makefile
-~/build/libdat/build/dat_store_libimpl test.dat build
-~/build/libdat/build/dat_read_libimpl test.dat Makefile
+cmake .
+cmake --build .
+./dat_store test.dat build
+./dat_read test.dat Makefile
+./dat_store_libimpl test.dat build
+./dat_read_libimpl test.dat Makefile
